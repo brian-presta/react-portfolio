3ac6b15f0dc868657d594126253b472457884d21
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-01 12:15:08 -0700

    component setup

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import About from './About'
+import Portfolio from './Portfolio'
+import Contact from './Contact'
+import Resume from './Resume'
+
+export default {
+    Navbar,
+    About,
+    Portfolio,
+    Contact,
+    Resume
+}
