c9015532c3fb0ec4ff297bd8161bbcfe32066d5d
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-02 13:22:32 -0700

    set up basic navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Nav } from 'react-bootstrap'
+import BootstrapNavbar from 'react-bootstrap/Navbar'
 
 function Navbar() {
 
     return(
-        <div>
-
-        </div>
+        <BootstrapNavbar expand="lg" className="container">
+            <BootstrapNavbar.Brand>Brian Presta</BootstrapNavbar.Brand>
+                <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+                <BootstrapNavbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#">About</Nav.Link>
+                        <Nav.Link href="#">Portfolio</Nav.Link>
+                        <Nav.Link href="#">Resume</Nav.Link>
+                        <Nav.Link href="#">Contact</Nav.Link>
+                    </Nav>
+                </BootstrapNavbar.Collapse>
+        </BootstrapNavbar>
     )
 }
 
