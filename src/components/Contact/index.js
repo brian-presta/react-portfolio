5ba49a852ec37dd65af2fdb041806a3673ba5955
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-05 16:50:26 -0700

    set up validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,12 +11,14 @@ function Contact() {
             return
         }
         setValidated(true)
+        alert("Message submitted! (or it would be, if this form were hooked up to anything)")
+        window.location.reload()
     }
     function runValidation(event) {
-        event.target.closest('form').classList.add("was-validated")
+        event.target.closest('.form-group').classList.add("was-validated")
     }
     return(
-        <Form validated={validated} className="mt-2" onSubmit={hanldeSubmit}>
+        <Form noValidate validated={validated} className="mt-2" onSubmit={hanldeSubmit}>
             <Form.Group controlId="formName">
                 <Form.Label className="font-weight-bold">Your Name:</Form.Label>
                 <Form.Control 
@@ -29,11 +31,24 @@ function Contact() {
             </Form.Group>
             <Form.Group controlId="formEmail">
                 <Form.Label className="font-weight-bold">Your Email:</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email"/>
+                <Form.Control 
+                required
+                type="email" 
+                placeholder="Enter your email"
+                onBlur={runValidation}
+                />
+                <Form.Control.Feedback type="invalid">A valid email is required</Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="formMessage">
                 <Form.Label className="font-weight-bold">Message:</Form.Label>
-                <Form.Control as="textarea" rows={4}placeholder="Enter your message"/>
+                <Form.Control
+                required
+                as="textarea" 
+                 rows={4}
+                 placeholder="Enter your message"
+                 onBlur={runValidation}
+                 />
+                 <Form.Control.Feedback type="invalid">This field is required</Form.Control.Feedback>
             </Form.Group>
             <Button type="submit">Submit</Button>
         </Form>
