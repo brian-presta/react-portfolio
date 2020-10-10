6a1c733fe3becfc94a00077357ece1cda3f9c4fd
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-10 16:46:42 -0700

    Updates

---
+++ precache-manifest.38dd2345596191e475f3755ae8b4d497.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3835cd06542f1295da3044da763a7380",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "743274523c14c989864e",
+    "url": "/react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "fcb2d515361c973252db",
+    "url": "/react-portfolio/static/css/main.19b8adc7.chunk.css"
+  },
+  {
+    "revision": "743274523c14c989864e",
+    "url": "/react-portfolio/static/js/2.fc96ff5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.fc96ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb2d515361c973252db",
+    "url": "/react-portfolio/static/js/main.3a327121.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6ef48a42814fdb17a5aeb11b654559e0",
+    "url": "/react-portfolio/static/media/food-festival.6ef48a42.PNG"
+  },
+  {
+    "revision": "4ec70dee6ee6bfbe137bf37bcc075bbe",
+    "url": "/react-portfolio/static/media/me.4ec70dee.jpg"
+  },
+  {
+    "revision": "a0623ae1dda33a7ca93ea7240d2bd111",
+    "url": "/react-portfolio/static/media/movie-finder.a0623ae1.PNG"
+  },
+  {
+    "revision": "eb8e48064e6eed0b1ba8529884ff84b6",
+    "url": "/react-portfolio/static/media/note-taker.eb8e4806.PNG"
+  },
+  {
+    "revision": "ad534324d9e361afbf8bcfb98549b7d6",
+    "url": "/react-portfolio/static/media/parrot.ad534324.jpg"
+  },
+  {
+    "revision": "aee19a32a51d9e54ba4f97ef07ab29a8",
+    "url": "/react-portfolio/static/media/photo-port.aee19a32.PNG"
+  },
+  {
+    "revision": "5cdd91ff7c17af6e8e73d63441943653",
+    "url": "/react-portfolio/static/media/run-buddy.5cdd91ff.PNG"
+  },
+  {
+    "revision": "973692c16e0bd8944758a53a2f0c79a7",
+    "url": "/react-portfolio/static/media/safeTravels.973692c1.PNG"
+  }
+]);
