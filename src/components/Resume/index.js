c38b4f3f11a7b9460130315293003e17bb5a9586
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-10 13:40:01 -0700

    set up resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,7 +6,25 @@ function Resume() {
         window.alert("Resume link is currently a placeholder!")
     }
     return(
-        <p>You can download my resume <a href="./my-resume.pdf" onClick={resumeHanlder}>here</a></p>
+        <section id="resume">
+            <p className="mt-3">You can download my resume <a href="./my-resume.pdf" onClick={resumeHanlder}>here</a>.</p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>- HTML/CSS/JS</li>
+                <li>- React</li>
+                <li>- Handlebars</li>
+                <li>- JQuery</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>- Node.js</li>
+                <li>- Express.js</li>
+                <li>- SQL</li>
+                <li>- MongoDB</li>
+                <li>- Writing RESTful APIs</li>
+                <li>- Writing Graph QL APIs</li>
+            </ul>
+        </section>
     )
 }
 
